2aa80fc7375d213fdbe07477022dc64673386b62
delacyr <delacyr@gmail.com>
2017-11-27 11:26:52 -0300

    making stateless components

--- src/pages/issues/components/Issue/index.js
+++ src/pages/issues/components/Issue/index.js
@@ -1,5 +1,5 @@
 /* Core */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 /* Presentational */
@@ -7,45 +7,43 @@ import { View, Text, TouchableOpacity, Linking, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
 
-export default class Issue extends Component {
-  static propTypes = {
-    issue: PropTypes.shape({
-      title: PropTypes.string,
-      html_url: PropTypes.string,
-      user: PropTypes.object,
-      avatarUrl: PropTypes.string,
-    }).isRequired,
-  };
+const Issue = ({ issue }) => {
+  const {
+    title,
+    html_url,
+    user: { login: user },
+    user: { avatar_url: avatarUrl },
+  } = issue;
 
-  render() {
-    const { issue } = this.props;
-
-    const {
-      title,
-      html_url,
-      user: { login: user },
-      user: { avatar_url: avatarUrl },
-    } = issue;
-
-    return (
-      <View style={styles.container}>
-        <Image
-          style={styles.icon}
-          source={{ uri: avatarUrl }}
-        />
-        <View style={styles.issueContainer}>
-          <Text numberOfLines={1} style={styles.title}>{title}</Text>
-          <Text style={styles.description}>{user}</Text>
-        </View>
-        <TouchableOpacity
-          onPress={() => {
-            Linking.openURL(html_url).catch(err => console.error('An error occurred', err));
-            }}
-        >
-          <Icon style={styles.button} name="chevron-right" size={20} color="#333" />
-        </TouchableOpacity>
-
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.icon}
+        source={{ uri: avatarUrl }}
+      />
+      <View style={styles.issueContainer}>
+        <Text numberOfLines={1} style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{user}</Text>
       </View>
-    );
-  }
-}
+      <TouchableOpacity
+        onPress={() => {
+          Linking.openURL(html_url).catch(err => console.error('An error occurred', err));
+          }}
+      >
+        <Icon style={styles.button} name="chevron-right" size={20} color="#333" />
+      </TouchableOpacity>
+
+    </View>
+  );
+};
+
+Issue.propTypes = {
+  issue: PropTypes.shape({
+    title: PropTypes.string,
+    html_url: PropTypes.string,
+    user: PropTypes.object,
+    avatarUrl: PropTypes.string,
+  }).isRequired,
+};
+
+export default Issue;
--- src/pages/repositories/components/Repository/index.js
+++ src/pages/repositories/components/Repository/index.js
@@ -1,45 +1,40 @@
 /* Core */
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 /* Presentational */
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import PropTypes from 'prop-types';
+import { colors } from 'styles';
 import styles from './styles';
 
-export default class Repository extends Component {
-  static propTypes = {
-    repository: PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      organization: PropTypes.string,
-      avatarUrl: PropTypes.string,
-    }).isRequired,
-    navigation: PropTypes.shape({
-      navigate: PropTypes.func,
-      setParams: PropTypes.func,
-    }).isRequired,
-  };
+const Repository = ({ repository, navigation }) => (
+  <View style={styles.container}>
+    <Image
+      style={styles.icon}
+      source={{ uri: repository.avatarUrl }}
+    />
+    <View style={styles.repoContainer}>
+      <Text style={styles.title}>{repository.name}</Text>
+      <Text style={styles.description}>{repository.organization}</Text>
+    </View>
+    <TouchableOpacity
+      onPress={() => navigation.navigate('Issues', { name: repository.name, organization: repository.organization })}
+    >
+      <Icon style={styles.button} name="chevron-right" size={20} color={colors.darker} />
+    </TouchableOpacity>
+  </View>
+);
 
-  render() {
-    const { repository } = this.props;
+Repository.propTypes = {
+  repository: PropTypes.shape({
+    name: PropTypes.string,
+    organization: PropTypes.string,
+    avatarUrl: PropTypes.string,
+  }).isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
 
-    return (
-      <View style={styles.container}>
-        <Image
-          style={styles.icon}
-          source={{ uri: repository.avatarUrl }}
-        />
-        <View style={styles.repoContainer}>
-          <Text style={styles.title}>{repository.name}</Text>
-          <Text style={styles.description}>{repository.organization}</Text>
-        </View>
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('Issues', { name: repository.name, organization: repository.organization })}
-        >
-          <Icon style={styles.button} name="chevron-right" size={20} color="#333" />
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+export default Repository;
